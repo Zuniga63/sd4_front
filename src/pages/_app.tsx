b8964e9ed8740efc20ada3abdb81fa9c39cab354
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-12-05 21:16:20 -0500

    refactor: add method for tailwind class dark

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,33 +10,47 @@ import {
 import { useLocalStorage, useHotkeys } from '@mantine/hooks';
 import { Provider } from 'react-redux';
 import { store } from 'src/store';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
   // --------------------------------------------------------------------------
   // MANTINE CONFIG
   // --------------------------------------------------------------------------
   emCache(); // Cache for use mantine with tailwind
+  const KEY_THEME = 'mantine-color-scheme';
 
   const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
-    key: 'mantine-color-scheme',
+    key: KEY_THEME,
     defaultValue: 'dark',
     getInitialValueInEffect: true,
   });
 
+  const setGlobalDarkTheme = (value?: ColorScheme) => {
+    let theme: ColorScheme = value || 'dark';
+
+    if (!value && window) {
+      const localTheme = JSON.parse(localStorage[KEY_THEME] || null);
+      if (localTheme && localTheme === 'light') theme = 'light';
+    }
+
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  };
+
   const toggleColorScheme = (value?: ColorScheme) => {
     const theme = value || (colorScheme === 'dark' ? 'light' : 'dark');
-
-    if (window) {
-      if (theme === 'dark') {
-        document.documentElement.classList.add('dark');
-      } else {
-        document.documentElement.classList.remove('dark');
-      }
-    }
     setColorScheme(theme);
+    setGlobalDarkTheme(theme);
   };
 
   useHotkeys([['mod+J', () => toggleColorScheme()]]);
+
+  useEffect(() => {
+    setGlobalDarkTheme();
+  }, []);
 
   return (
     <ColorSchemeProvider
@@ -48,7 +62,6 @@ export default function App({ Component, pageProps }: AppProps) {
         theme={{
           colorScheme,
           fontFamily: 'inherit',
-          loader: 'bars',
         }}
       >
         <Provider store={store}>
