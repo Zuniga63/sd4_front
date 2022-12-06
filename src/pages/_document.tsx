9e062080d817c6dc89e1ddc79b82740cf8bea50c
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-12-05 21:11:21 -0500

    chore: set the shorcur icon and favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,13 +8,18 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang="es">
+      <Html lang="es" className="dark">
         <Head>
           <link
             href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&family=Roboto:ital,wght@0,400;0,500;0,700;1,400&family=Zen+Dots&display=swap"
             rel="stylesheet"
           />
-          <link rel="shortcut icon" href="images/logo_62601199d793d.png" type="image/x-icon" />
+          <link
+            rel="shortcut icon"
+            href={process.env.NEXT_PUBLIC_BRAND_LOGO}
+            type="image/x-icon"
+          />
+          <link rel="icon" href={process.env.BRAND_LOGO} type="image/x-icon" />
         </Head>
         <body className="dark:bg-defaul-body">
           <Main />
