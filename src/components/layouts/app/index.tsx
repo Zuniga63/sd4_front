d64012612208d7b89a55a48210f6501beb358fbe
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-12-08 11:33:55 -0500

    feat: add portal and affix buttom

--- src/components/layouts/app/index.tsx
+++ src/components/layouts/app/index.tsx
@@ -4,6 +4,9 @@ import { useAppSelector } from 'src/store/hooks';
 import Head from 'next/head';
 import AppHeader from '../AppHeader';
 import AppSidebar from '../AppSidebar';
+import { Affix, Button, Portal, Transition } from '@mantine/core';
+import { useWindowScroll } from '@mantine/hooks';
+import { IconArrowUp } from '@tabler/icons';
 
 interface Props {
   title?: string;
@@ -14,6 +17,7 @@ interface Props {
 const AppLayout = ({ title = 'App Layout', description, children }: Props) => {
   const { name: appName } = useAppSelector(appSelector);
   const fullTitle = appName ? `${title} - ${appName}` : title;
+  const [scroll, scrollTo] = useWindowScroll();
 
   return (
     <>
@@ -24,7 +28,23 @@ const AppLayout = ({ title = 'App Layout', description, children }: Props) => {
 
       <AppHeader />
       <main>{children}</main>
-      <AppSidebar>Foo</AppSidebar>
+      <Portal>
+        <AppSidebar>Foo</AppSidebar>
+        <Affix position={{ bottom: 20, right: 20 }}>
+          <Transition transition="slide-up" mounted={scroll.y > 0}>
+            {transitionStyles => (
+              <Button
+                leftIcon={<IconArrowUp size={16} />}
+                style={transitionStyles}
+                onClick={() => scrollTo({ y: 0 })}
+                radius="xl"
+              >
+                Ir arriba
+              </Button>
+            )}
+          </Transition>
+        </Affix>
+      </Portal>
     </>
   );
 };
